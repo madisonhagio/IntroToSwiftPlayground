6cbe81297bac652e375d850408e63ca4173bdc09
madisonhagio <hagiom@hawaii.edu>
2022-01-26 16:32:51 -1000

    Assignment #2
    Introduction to True and False

--- SwiftPlaygroundsOne.playground/Contents.swift
+++ SwiftPlaygroundsOne.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+// Assignment #2 Introduction to True and False
+var appState = false
+print(appState)
+var numberOne = 5
+var numberTwo = 7
+var comparison = numberOne > numberTwo
