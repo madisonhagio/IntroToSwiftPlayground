07b8060d7cdf797374d79beb8338e8b451a54309
madisonhagio <hagiom@hawaii.edu>
2022-01-26 18:00:36 -1000

    Finished Swift Playgrounds One Assignment

--- SwiftPlaygroundsOne.playground/Contents.swift
+++ SwiftPlaygroundsOne.playground/Contents.swift
@@ -8,3 +8,24 @@ print(appState)
 var numberOne = 5
 var numberTwo = 7
 var comparison = numberOne > numberTwo
+
+var myFirstVariable: String
+myFirstVariable = "I've created my first variable"
+
+//we're going to do something with strings
+var mySecondVariable = "and look I added two strings together"
+
+myFirstVariable + mySecondVariable
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+print(myThirdVariable)
+
+var HelloMyName: String
+HelloMyName = "I've created a new variable"
+
+var NewVariable = "and look I added two string together"
+
+HelloMyName + NewVariable
+
+var myFourthVariable = HelloMyName + NewVariable
+print(myFourthVariable)
